a030e01e947d4b385b75128e515806aa5e7a260c
ali mohebbi <ali.mohebbi@usi.ch>
2021-02-02 14:34:43 +0100

    update script

--- angular-image/restart.sh
+++ angular-image/restart.sh
@@ -2,7 +2,7 @@
 
 docker rm myangular
 docker image rm angularimage:latest
-docker build --tag angularimage .
+docker build --tag alimohebbi/angular .
 docker volume rm starlab-ui
 docker volume create --name starlab-ui
 docker run -it --name myangular -v starlab-ui:/starlab-ui alimohebbi/angular
--- gunicorn-image/restart-local.sh
+++ gunicorn-image/restart-local.sh
@@ -6,8 +6,8 @@ docker network rm mynet
 docker network create mynet
 docker build --tag alimohebbi/guni .
 docker run --name guni-c -d -p 8000:8000 \
--v /Users/usiusi/Documents/docker-start/gunicorn-image/media:/media \
--v /Users/usiusi/Documents/docker-start/gunicorn-image/static:/static \
--v /Users/usiusi/Documents/docker-start/gunicorn-image/database/:/database --network mynet alimohebbi/guni
+-v /Users/usiusi/Documents/Repositories/star_dockers/gunicorn-image/media:/media \
+-v /Users/usiusi/Documents/Repositories/star_dockers/gunicorn-image/static:/static \
+-v /Users/usiusi/Documents/Repositories/star_dockers/gunicorn-image/database/:/database --network mynet alimohebbi/guni
 
 

94849fb169de0d94b5ae21cc7fb86090d2c2e066
ali mohebbi <ali.mohebbi@usi.ch>
2021-02-03 11:36:28 +0100

    updates

--- angular-image/restart.sh
+++ angular-image/restart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
 docker rm myangular
-docker image rm angularimage:latest
+docker image rm angular:latest
 docker build --tag alimohebbi/angular .
 docker volume rm starlab-ui
 docker volume create --name starlab-ui
-docker run -it --name myangular -v starlab-ui:/starlab-ui alimohebbi/angular
+docker run -it --name myangular -v starlab-ui:/starlab-ui alimohebbi/angular-c
 
 

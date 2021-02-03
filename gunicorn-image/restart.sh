6ca8c3750e8a0f305d071c77fb68fa1f06359031
ali mohebbi <ali.mohebbi@usi.ch>
2021-02-03 12:35:40 +0100

    updates

--- gunicorn-image/restart.sh
+++ gunicorn-image/restart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
 docker rm guni-c
-docker image rm guniimage:latest
+docker image rm alimohebbi/guni:latest
 docker network rm mynet
 docker network create mynet
-docker build --tag guniimage .
-docker run --name guni-c -d -p 8000:8000 \
+docker build --tag alimohebbi/guni .
+docker run --name guni-c  -p 8000:8000 \
 -v /home/star/media:/media \
 -v /home/star/static:/static \
 -v /home/star/database/:/database --network mynet alimohebbi/guni
